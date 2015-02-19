0ace2f77e4b025383b7b2a22ce8fc7edb2d46b60
Timofey Cherniavsky <varloc2000@gmail.com>
2015-02-19 12:28:18 +0300

    Add capifony deploy configuration. Add phpstorm folder to gitignore.

---
+++ app/config/deploy.rb
@@ -0,0 +1,10 @@
+#APP
+set :application, "Insider"
+set :app_path, "."
+set :scm, :none
+
+#FTP
+set :login, "varloc2000"
+set :password, "Varloc80177413"
+set :ftp_host, "93.85.85.197"
+set :deploy_to, "/www/insider.rats-records.by"
